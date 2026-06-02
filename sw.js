f24c1d789d6245d2bd6c4556d36eb81f86498963
Maa Nirmala DJ & Tent House <maa.nirmala.dj.beltikri@gmail.com>
2026-06-02 12:03:25 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('mnd-store').then((cache) => cache.addAll([
+      '/',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
